8ddd80e88293205747c31a40231c61cd11c4b70a
Takahashi-Shinobu-Fukuyama <shinobu-takahashi@anabuki.ac.jp>
2018-06-15 19:21:36 +0900

    Add SkyBox Sample

---
+++ SkyBoxSamplePushIKKey/3DProgramming/Define.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define P_SPEED			0.3f	//移動速度（プレイヤー）	0.3f
+#define P_HP			500.0f	//HP（プレイヤー）			500.0f
+#define P_FB			10		//発射間隔（プレイヤー）	10
+
+#define E_SPEED			0.2f	//移動速度（敵）	0.2f
+#define E_HP			500.0f	//HP（敵）			500.0f
+#define E_FB			100		//発射間隔（敵）	100
+
+#define P_BEAM_SPEED	1.0f	//ビームの移動速度（プレイヤー）
+#define P_BEAM_HP		50		//ビームの威力（プレイヤー）
+#define P_BEAM_RANGE	50.0f	//ビーム射程距離			50.0f
+
+#define E_BEAM_SPEED	1.0f	//ビームの移動速度（プレイヤー）
+#define E_BEAM_HP		50		//ビームの威力（プレイヤー）
+#define E_BEAM_RANGE	50.0f	//ビーム射程距離			50.0f
+
+#define RESPAWN_TIME	300		//リスポン時間
+
+
